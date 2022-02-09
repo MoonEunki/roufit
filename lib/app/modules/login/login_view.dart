11d1857e94777b0d3a28264abc8345cd7b1dbd03
MEK <ansenfrl@gmail.com>
2022-02-09 23:47:04 +0900

    구글로그인 버튼 변경

--- lib/app/modules/login/login_view.dart
+++ lib/app/modules/login/login_view.dart
@@ -11,18 +11,49 @@ class LoginView extends GetView<LoginController> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Container(
-                child: Text('Roufit', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
-              ),
+              Text('Roufit', style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold)),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text('3초만에 roufit 로그인!'),
-                  ElevatedButton(child: Text('구글로그인하기'), onPressed: () => controller.login()),
+                  Text('3초만에 roufit 로그인하기'),
+                  SizedBox(height: 10),
+                  GestureDetector(onTap: () => controller.login(), child: GoogleLoginButton()),
                 ],
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class GoogleLoginButton extends StatelessWidget {
+  const GoogleLoginButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Color(0xFF4285F4), borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage("assets/images/3.0x/google-logo.png"),
+              height: 24.0,
+              color: Colors.white,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.w600),
+              ),
+            )
+          ],
         ),
       ),
     );
